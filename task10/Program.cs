6f4705849c298d9159230b2f9d19b709db5a6c52
imAlinaa <alina.btkv@yandex.ru>
2022-12-17 12:01:24 +0700

    Задания 10, 15

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+a = (a / 10) % 10;
+
+Console.WriteLine($"Вторая цифра числа = {a}");
---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+if (numberDay < 1 || numberDay > 7)
+{
+    Console.WriteLine("Вы ввели некорректное число!");
+}
+else if (numberDay > 5 && numberDay <= 7)
+{
+    Console.WriteLine("Это выходной день!");
+}
+else
+{
+    Console.WriteLine("Это не выходной день.");
+}
